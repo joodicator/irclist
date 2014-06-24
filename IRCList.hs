76ddf67144837774aa383cf38ad348d56ac57984
Joseph Crowe <joseph.crowe@live.co.uk>
2014-06-24 12:23:50 +0100

    Completed main functionality.

--- IRCList.hs
+++ IRCList.hs
@@ -25,7 +25,14 @@ ircList' = do
     host <- asks cHost; port <- asks cPort
     h <- lift $ connectTo host (PortNumber port)
     login h
-    getList h
+    l <- getList h
+    lift $ ehPutStrLn h "QUIT"
+    lift $ hClose h
+
+    let l'  = sortBy (\(_,m,_) (_,n,_) -> compare n m) l
+    let l'' = map (\(c,n,t) -> [c, show n, t]) l'
+    forM_ l'' $ \r -> do
+        lift $ putStrLn $ intercalate "\t" r
 
 login :: Handle -> ReaderT Conf IO ()
 login h = do
@@ -42,7 +49,7 @@ login' h = do
         (_,"432",_) -> lift exitFailure -- ERR_ERRONEUSNICKNAME
         (_,"433",_) -> login'' h        -- ERR_NICKNAMEINUSE
         (_,"436",_) -> login'' h        -- ERR_NICKCOLLISION
-        _           -> login' h
+        m           -> misc m h >> login' h
 
 login'' :: Handle -> ReaderT Conf IO ()
 login'' h = do
@@ -50,9 +57,26 @@ login'' h = do
     lift $ ehPutStrLn h $ unwords ["NICK", nick]
     withReaderT (\c -> c{cNick=nicks}) (login' h)
 
-getList :: Handle -> ReaderT Conf IO ()
+getList :: Handle -> ReaderT Conf IO [(String,Integer,String)]
 getList h = do
-    void $ forever $ lift (ehGetLine h)
+    lift $ ehPutStrLn h "LIST"
+    l <- getList' h
+    return [(chan, read count, unwords topic) | chan : count : topic <- l]
+
+getList' :: Handle -> ReaderT Conf IO [[String]]
+getList' h = do
+    msg <- lift $ parseMessage `fmap` ehGetLine h
+    case msg of
+        (_,"322",_:a) -> do -- RPL_LIST
+            l <- getList' h
+            return (a : l)
+        (_,"323",_) -> do -- RPL_LISTEND
+            return []
+        m -> misc m h >> getList' h
+
+misc :: (Maybe String, String, [String]) -> Handle -> ReaderT Conf IO ()
+misc (_,"PING",a) h = lift $ ehPutStrLn h $ unwords ["PONG", ':' : unwords a]
+misc _            _ = return ()
 
 parseMessage :: String -> (Maybe String, String, [String])
 parseMessage (':':l)
@@ -67,10 +91,10 @@ parseMessage l
 ehPutStrLn :: Handle -> String -> IO ()
 ehPutStrLn h s = do
     hPutStrLn h s
-    putStrLn $ "\27[33m< " ++ s ++ "\27[0m"
+    hPutStrLn stderr $ "\27[33m< " ++ s ++ "\27[0m"
 
 ehGetLine :: Handle -> IO String
 ehGetLine h = do
     s <- hGetLine h
-    putStrLn $ "\27[36m> " ++ s ++ "\27[0m"
+    hPutStrLn stderr $ "\27[36m> " ++ s ++ "\27[0m"
     return s
